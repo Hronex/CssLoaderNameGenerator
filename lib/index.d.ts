147ec8e66694609a5e197f91d07f64e126ac8e8c
Vyacheslav Demot <v.demot@gmail.com>
2021-04-20 10:33:22 +0300

    Updated code

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare class CssLoaderNameGenerator {
-    private readonly _maxLength;
-    private readonly _minLength;
-    private readonly usedNames;
-    private readonly cache;
-    constructor(minLength: number, maxLength: number);
-    private readonly randomNaming;
-    readonly getLocalIdent: (context: Record<string, unknown>, localIdentName: string, localName: string) => string;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,46 @@
-class CssLoaderNameGenerator {
-	private readonly _maxLength: number;
+module.exports = class CssLoaderNameGenerator {
 	private readonly _minLength: number;
-
-	private readonly usedNames: string[] = [];
-	private readonly cache: Record<string, string> = {};
+	private readonly _maxLength: number;
+	private readonly _usedNames: string[] = [];
+	private readonly _cache: Record<string, string> = {};
+	private readonly _chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_';
+	private readonly _fchars = 'abcdefghjklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_';
 
 	constructor(minLength: number, maxLength: number) {
-		this._maxLength = maxLength;
 		this._minLength = minLength;
+		this._maxLength = maxLength;
 	}
 
 	private readonly randomNaming = (): string => {
 		let max = this._minLength,
 			result = '';
-		const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_',
-			fchars = 'abcdefghjklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_';
 
 		do {
-			if (this.usedNames.length >= Math.pow(64, max - 1) * 52 && max >= this._maxLength) {
-				return 'OutOfPossibility';
+			if (this._usedNames.length >= Math.pow(64, max - 1) * 52 && max >= this._maxLength) {
+				return 'Out of range - increase max length';
 			}
 
-			if (this.usedNames.length >= Math.pow(64, max - 1) * 52 && max < this._maxLength) {
+			if (this._usedNames.length >= Math.pow(64, max - 1) * 52 && max < this._maxLength) {
 				++max;
 			}
 
-			result = '';
-			result += fchars[Math.floor(Math.random() * fchars.length)];
+			result += this._fchars[Math.floor(Math.random() * this._fchars.length)];
 
 			for (let i = max - 1; i > 0; --i) {
-				result += chars[Math.floor(Math.random() * chars.length)];
+				result += this._chars[Math.floor(Math.random() * this._chars.length)];
 			}
-		} while (this.usedNames.includes(result));
+		} while (this._usedNames.includes(result));
 
-		this.usedNames.push(result);
+		this._usedNames.push(result);
 
 		return result;
 	};
 
-	public readonly getLocalIdent = (context: Record<string, unknown>, localIdentName: string, localName: string): string => {
+	public readonly getLocalIdent = (
+		context: Record<string, unknown>,
+		localIdentName: string,
+		localName: string,
+	): string => {
 		let randName = this.randomNaming();
 
 		if (/^i-/i.test(localName)) {
@@ -47,13 +49,10 @@ class CssLoaderNameGenerator {
 			randName = 'i_';
 		}
 
-		if (typeof this.cache[localName] == 'undefined') {
-			this.cache[localName] = randName;
-
-			return this.cache[localName];
+		if (typeof this._cache[localName] == 'undefined') {
+			this._cache[localName] = randName;
 		}
 
-		return this.cache[localName];
+		return this._cache[localName];
 	};
-}
-module.exports = CssLoaderNameGenerator;
+};
